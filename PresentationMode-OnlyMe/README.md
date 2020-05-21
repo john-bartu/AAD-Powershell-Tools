5d9ff0202a635ff5b323c5f7e08b554b5edea8c5
Jan Bartula <bartula.jan@gmail.com>
2020-05-21 20:41:29 +0200

    default role of meeting attendee

---
+++ PresentationMode-OnlyMe/README.md
@@ -0,0 +1,5 @@
+# Who can present meeting attendee
+
+Add a global admin setting for the default role of meeting attendee.
+
+`DesignatedPresenterRoleMode`
---
+++ PresentationMode-OnlyMe/presentator-only-me.ps1
@@ -0,0 +1,8 @@
+﻿#Import-Module SkypeOnlineConnector
+#$sfbSession = New-CsOnlineSession
+#Import-PSSession $sfbSession
+
+
+Get-CsTeamsMeetingPolicy -Identity Global
+
+#Set-CsTeamsMeetingPolicy -Identity Global -DesignatedPresenterRoleMode OrganizerOnlyUserOverride
